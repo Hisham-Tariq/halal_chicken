fa50f259a5ea57a3a1b64567b8461b921b1a00a1
Iqra-Munir <iqramunir.hi@gmail.comgit config --global user.name Iqra-Munirgit config --global user.email iqramunir.hi@gmail.com>
2022-12-18 13:02:14 +0500

    contact us

--- resources/views/client/views/contact_us.blade.php
+++ resources/views/client/views/contact_us.blade.php
@@ -4,21 +4,23 @@
 
 <!--====Breadcumb
 ============================== -->
-    <div class="breadcumb-wrapper " data-bg-src="assets/halalchickenimages/banners/banner5.jpg">
-        <div class="container z-index-common">
-            <div class="breadcumb-content">
-                <h1 class="breadcumb-title text-white">Contact <span class="font-style text-theme2">Us</span></h1>
-                <ul class="breadcumb-menu text-white">
-                    <li><a href="index.html" class="text-white">Home</a></li>
-                    <li class="text-white">Contact Us</li>
-                </ul>
+    <div class=" " data-bg-src="assets/halalchickenimages/banners/banner5.jpg">
+        <div class="breadcumb-wrapper" style="background-color: rgba(0,0,0,0.6)">
+            <div class="container z-index-common">
+                <div class="breadcumb-content">
+                    <h1 class="breadcumb-title text-white">Contact <span class="font-style text-theme2">Us</span></h1>
+                    <ul class="breadcumb-menu text-white">
+                        <li><a href="index.html" class="text-white">Home</a></li>
+                        <li class="text-white">Contact Us</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
     <!--==============================
 Map Area  
 ==============================-->
-    <div class="space" data-bg-src="assets/halalchickenimages/banners/banner6.jpeg" style="background-color: rgba(0, 0, 0, 0.9)">
+    <div class="space" style="background-color: rgba(0, 0, 0, 0.9)">
         <div class="container">
             <div class="contact-map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7310056272386!2d89.2286059153658!3d24.00527418490799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe9b97badc6151%3A0x30b048c9fb2129bc!2sAngfuzsoft!5e0!3m2!1sen!2sbd!4v1651028958211!5m2!1sen!2sbd" allowfullscreen="" loading="lazy"></iframe>
@@ -28,7 +30,7 @@ Map Area
     <!--==============================
 Contact Area  
 ==============================-->
-    <div data-pos-for="#contactFormSec" data-sec-pos="bottom-half">
+    <div data-pos-for="#contactFormSec" data-sec-pos="" style="background-color: rgba(0,0,0,1);">
         <div class="container">
             <div class="row gy-4">
                 <div class="col-lg-4">
@@ -67,7 +69,8 @@ Contact Area
             </div>
         </div>
     </div>
-    <section class="space" id="contactFormSec" style="background-color: rgba(0,0,0,0.6)">
+    <section id="contactFormSec" data-bg-src="assets/halalchickenimages/banners/foodbanner4.jpg">
+       <div class="space"   style="background-color: rgba(0,0,0,0.8)">
         <div class="container" >
             <form action="mail.php" method="POST" class="contact-form ajax-contact" style="background-color: black">
                 <h3 class="form-title">Get In Touch</h3>
@@ -105,6 +108,8 @@ Contact Area
                 <p class="form-messages mb-0 mt-3"></p>
             </form>
         </div>
+       </div>
+
     </section>
 
 @stop
